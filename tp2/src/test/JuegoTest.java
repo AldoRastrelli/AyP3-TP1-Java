1ecb986da605fa68f01029a4465ee1e397533037
Josefina <jiterman@fi.uba.ar>
2020-07-21 20:11:46 -0300

    Corrección de pruebas

---
+++ tp2/src/test/JuegoTest.java
@@ -0,0 +1,15 @@
+package test;
+
+import org.junit.Test;
+
+public class JuegoTest {
+    @Test
+    public void PreguntaVoFClasicoPuedeCrearseIndicandoRespuestaCorrectaTest(){
+
+    }
+
+    @Test
+    public void PreguntaVoFClasicoRecibeListRtasYAsignaCorrectamentePuntosAJugadoresTest(){
+
+    }
+}
