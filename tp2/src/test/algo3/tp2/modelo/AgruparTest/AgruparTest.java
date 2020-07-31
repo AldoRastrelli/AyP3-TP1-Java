dffa2cec3be88bdbf94bc323120ff04414f272c5
ignaciojaneiro <39175112+ignaciojaneiro@users.noreply.github.com>
2020-07-31 20:34:42 -0300

    Agrupar Test

--- tp2/src/test/algo3/tp2/modelo/AgruparTest/AgruparTest.java
+++ tp2/src/test/algo3/tp2/modelo/AgruparTest/AgruparTest.java
@@ -14,4 +14,49 @@ import static org.junit.Assert.assertTrue;
 
 public class AgruparTest {
 
+    @Test
+    public void PreguntaAgruparClasicoPuedeCrearseIndicandoRespuestaCorrectaTest() {
+
+        List<String> respuestaCorrecta = new ArrayList<>();
+        respuestaCorrecta.add("Pan");
+        respuestaCorrecta.add("Hamburguesa");
+        respuestaCorrecta.add("0");
+        respuestaCorrecta.add("Agua");
+        respuestaCorrecta.add("Coca-Cola");
+        respuestaCorrecta.add("0");
+
+        FactoryPreguntas factory = new FactoryPreguntas();
+        Pregunta pregunta = factory.Agrupar(respuestaCorrecta);
+
+        assertTrue(((Agrupar) pregunta).esRespuestaCorrecta(respuestaCorrecta));
+    }
+
+    @Test
+    public void PreguntaMultipleClasicoRecibeListRtasYAsignaCorrectamentePuntosAJugadoresTest(){
+
+        var juego = new Juego();
+
+        juego.crearJugador("Marcos");
+        juego.crearJugador("Evelyn");
+
+        List<String> respuestaCorrecta = new ArrayList<String>(){{ add("Pan"); add("Hamburguesa"); add("0"); add("Agua"); add("Coca-Cola"); add("0");}};
+        List<String> respuestaIncorrecta = new ArrayList<String>(){{ add("Agua"); add("Hamburguesa"); add("0"); add("Pan"); add("Coca-Cola"); add("0"); }};
+        FactoryPreguntas factory = new FactoryPreguntas();
+        Pregunta pregunta = factory.Agrupar(respuestaCorrecta);
+
+        Map<String,List<String>> respuestas = new HashMap<String,List<String>>(){{
+            put("Marcos",respuestaIncorrecta);
+            put("Evelyn",respuestaCorrecta);
+        }};
+
+        Map<String,Integer> puntajeEsperado = new HashMap<String,Integer>(){{
+            put("Marcos",0);
+            put("Evelyn",1);
+        }};
+
+        Map<String, Integer> puntajeObtenido = pregunta.determinarPuntaje(respuestas);
+
+        assertTrue(puntajeObtenido.equals(puntajeEsperado));
+
+    }
 }
