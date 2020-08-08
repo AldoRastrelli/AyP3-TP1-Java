4b6aff630c72b39ad77bed539d23de61da55313f
Aldana Rastrelli <aldanarastrelli@gmail.com>
2020-08-07 21:34:16 -0300

    pruebas exclusividad de puntaje OrdenarTest

--- src/test/modelo/OrdenarTest/OrdenarTest.java
+++ src/test/modelo/OrdenarTest/OrdenarTest.java
@@ -2,8 +2,10 @@ package test.modelo.OrdenarTest;
 
 import model.FactoryPreguntas.FactoryPreguntas;
 import model.Juego;
+import model.Jugador;
 import model.Preguntas.Ordenar;
 import model.Preguntas.Pregunta;
+import model.RondaActual;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -60,4 +62,99 @@ public class OrdenarTest {
         assertEquals(puntajeObtenido, puntajeEsperado);
     }
 
+    @Test
+    public void JugadorUsaExclusividadYSeAplicaCorrectamente(){
+        var juego = new Juego();
+
+        Jugador jugador1 = juego.crearJugador("Marcos");
+        Jugador jugador2 = juego.crearJugador("Evelyn");
+        RondaActual rondaActual = juego.crearRondaActual();
+
+        List<String> respuestaCorrecta = new ArrayList<String>(){{ add("1930"); add("1940"); add("1950"); add("1960");}};
+        List<String> respuestaIncorrecta = new ArrayList<String>(){{ add("1940"); add("1930"); add("1960"); add("1950");}};
+
+        FactoryPreguntas factory = new FactoryPreguntas();
+        Pregunta pregunta = factory.Ordenar("Ordenar de menor a mayor", respuestaCorrecta);
+
+        rondaActual.guardarRespuesta(jugador1.getNombre(), respuestaCorrecta, jugador1.elegirBoostExclusivo());
+        rondaActual.guardarRespuesta(jugador2.getNombre(), respuestaIncorrecta, jugador2.noUsarBoost());
+
+        juego.guardarPreguntaActual(pregunta);
+
+        juego.calcularPuntaje();
+
+        assertTrue(jugador1.getPuntos().equals(2));
+    }
+
+    @Test
+    public void JugadorUsaExclusividadNoVerificaCondicionesEntoncesNoSeAplica(){
+        var juego = new Juego();
+
+        Jugador jugador1 = juego.crearJugador("Marcos");
+        Jugador jugador2 = juego.crearJugador("Evelyn");
+        RondaActual rondaActual = juego.crearRondaActual();
+
+        List<String> respuestaCorrecta = new ArrayList<String>(){{ add("1930"); add("1940"); add("1950"); add("1960");}};
+        List<String> respuestaIncorrecta = new ArrayList<String>(){{ add("1940"); add("1930"); add("1960"); add("1950");}};
+
+        FactoryPreguntas factory = new FactoryPreguntas();
+        Pregunta pregunta = factory.Ordenar("Ordenar de menor a mayor", respuestaCorrecta);
+
+        rondaActual.guardarRespuesta(jugador1.getNombre(), respuestaCorrecta, jugador1.elegirBoostExclusivo());
+        rondaActual.guardarRespuesta(jugador2.getNombre(), respuestaCorrecta, jugador2.noUsarBoost());
+
+        juego.guardarPreguntaActual(pregunta);
+
+        juego.calcularPuntaje();
+
+        assertTrue(jugador1.getPuntos().equals(1));
+    }
+
+    @Test
+    public void JugadorUsaExclusividadSeEquivocaEntoncesSeAplicaAlOtroJugador(){
+        var juego = new Juego();
+
+        Jugador jugador1 = juego.crearJugador("Marcos");
+        Jugador jugador2 = juego.crearJugador("Evelyn");
+        RondaActual rondaActual = juego.crearRondaActual();
+
+        List<String> respuestaCorrecta = new ArrayList<String>(){{ add("1930"); add("1940"); add("1950"); add("1960");}};
+        List<String> respuestaIncorrecta = new ArrayList<String>(){{ add("1940"); add("1930"); add("1960"); add("1950");}};
+
+        FactoryPreguntas factory = new FactoryPreguntas();
+        Pregunta pregunta = factory.Ordenar("Ordenar de menor a mayor", respuestaCorrecta);
+
+        rondaActual.guardarRespuesta(jugador1.getNombre(), respuestaIncorrecta, jugador1.elegirBoostExclusivo());
+        rondaActual.guardarRespuesta(jugador2.getNombre(), respuestaCorrecta, jugador2.noUsarBoost());
+
+        juego.guardarPreguntaActual(pregunta);
+
+        juego.calcularPuntaje();
+
+        assertTrue(jugador2.getPuntos().equals(2) && jugador1.getPuntos().equals(0));
+    }
+
+    @Test
+    public void JugadoresUsanExclusividadSeDuplicaYSeAplicaCorrectamente(){
+        var juego = new Juego();
+
+        Jugador jugador1 = juego.crearJugador("Marcos");
+        Jugador jugador2 = juego.crearJugador("Evelyn");
+        RondaActual rondaActual = juego.crearRondaActual();
+
+        List<String> respuestaCorrecta = new ArrayList<String>(){{ add("1930"); add("1940"); add("1950"); add("1960");}};
+        List<String> respuestaIncorrecta = new ArrayList<String>(){{ add("1940"); add("1930"); add("1960"); add("1950");}};
+
+        FactoryPreguntas factory = new FactoryPreguntas();
+        Pregunta pregunta = factory.Ordenar("Ordenar de menor a mayor", respuestaCorrecta);
+
+        rondaActual.guardarRespuesta(jugador1.getNombre(), respuestaCorrecta, jugador1.elegirBoostExclusivo());
+        rondaActual.guardarRespuesta(jugador2.getNombre(), respuestaIncorrecta, jugador2.elegirBoostExclusivo());
+
+        juego.guardarPreguntaActual(pregunta);
+
+        juego.calcularPuntaje();
+
+        assertTrue(jugador1.getPuntos().equals(4));
+    }
 }
